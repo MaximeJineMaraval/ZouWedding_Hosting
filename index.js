633dcde0aa7c8f06e6c1aa29a3b09d2b01fe3cb7
Maxime Maraval <46402524+MaximeJineMaraval@users.noreply.github.com>
2024-06-06 17:51:22 +0200

    feat: make login page 100% functionnal

---
+++ index.js
@@ -0,0 +1,62 @@
+// Helpers
+const removeAccents = str => str.normalize('NFD').replace(/[\u0300-\u036f]/g, '')
+
+// API calls info
+const apiUrl = 'https://api.baserow.io/api/database/rows/table/302843/'
+const token = 'o0pJ9uzhCRFeCHA3KKCpWAiTw68oAR8C'
+
+// Views
+let loginButton = document.getElementById("loginButton")
+let nameField = document.getElementById("guestname")
+let resultsLabel = document.getElementById("results")
+
+// on nameField input
+nameField.addEventListener("input", function() {
+    loginButton.disabled = nameField.value === ""
+})
+
+// on loginButton click
+loginButton.addEventListener("click", function(){
+    const treatedName = removeAccents(nameField.value)
+    const names = treatedName.split(' ')
+
+    const params = new URLSearchParams()
+    params.append('user_field_names', true)
+    if(names.length == 1) {
+        params.append('filter__search__contains', names[0])
+    } else {
+        params.append('filter__search__contains', names[0]+names[1])
+        params.append('filter__search__contains', names[1]+names[0])
+    }
+    params.append('filter_type', 'OR')
+
+    const finalApiUrl = apiUrl + '?' + params.toString()
+
+    const requestOptions = {
+        method: 'GET',
+        headers: {
+            'Authorization': `Token ${token}`,
+          }
+    }
+
+    resultsLabel.textContent = "chargement..."
+    fetch(finalApiUrl, requestOptions)
+        .then(response => {
+            if(!response.ok) {
+                throw new Error('Network response was not ok')
+            }
+            return response.json()
+        })
+        .then(data => {
+            if(data.count === 0) {
+                resultsLabel.textContent = "Nous n'avons trouvé aucun invité à ce nom. Si le problème persiste, contacte directement Maxime ou Justine 🙂"
+            } else if (data.count > 1) {
+                resultsLabel.textContent = "Trop d'invités correspondent à ce nom. Si le problème persiste, contacte directement Maxime ou Justine 🙂"
+            } else {
+                window.location.href = "main.html"
+            }
+        })
+        .catch(error => {
+            resultsLabel.textContent = error
+        })
+})
