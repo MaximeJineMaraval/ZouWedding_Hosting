c29ef28f1005b9ba93f4b80ee675751d51ac7e7e
Maxime Maraval <46402524+MaximeJineMaraval@users.noreply.github.com>
2024-10-25 19:06:39 +0200

    feat: add animation for loader

--- index.js
+++ index.js
@@ -86,12 +86,23 @@ function showSnackbar(text) {
     }, 5000)
 }
 
-function showLoader(show) {
-    let display = "none"
-    if (show) { 
-        display = "flex"
+// Show loader with animation
+async function showLoader(show) {
+    const loaderContainer = document.getElementById("loaderContainer")
+    if(show) {
+        loaderContainer.style.display = "flex"
+        await delay(10)
+        loaderContainer.style.opacity = 1
+    } else {
+        loaderContainer.style.opacity = 0
+        await delay(500)
+        loaderContainer.style.display = "none"
     }
-    document.getElementById("loaderContainer").style.display = display
 }
 
-showLoader(false)
+// Simplify delay usage
+function delay(milliseconds){
+    return new Promise(resolve => {
+        setTimeout(resolve, milliseconds);
+    });
+}
