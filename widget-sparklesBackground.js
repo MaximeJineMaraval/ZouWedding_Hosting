0fce87dbfd722a9527b024fd7564cec2585e6fa6
Maxime Maraval <46402524+MaximeJineMaraval@users.noreply.github.com>
2024-10-30 20:29:12 +0100

    feat: add sparkles for planning section, screen transition and split css files

--- widget-sparklesBackground.js
+++ widget-sparklesBackground.js
@@ -14,26 +14,30 @@ function applySparklesBackground() {
         cols = Math.floor(window.innerWidth / 200);
     }
     console.log(window.innerWidth)
-    const sparkesBackground = document.getElementById('sparklesBackground');
-    for (let row = 0; row < rows; row++) {
-        for (let col = 0; col < cols; col++) {
-            const sparkles = document.createElement('img');
-            sparkles.style.position = "absolute";
-            sparkles.src = "res/background_sparkles.png"
-            sparkles.classList.add('sparkles');
     
-            // Define sparkles size
-            const size = 1;
-            sparkles.style.width = `${size}em`;
-            sparkles.style.height = `auto`;
-    
-            // Position the sparkles with random offset
-            const posX = (col / cols) * window.innerWidth + getRandomOffset();
-            const posY = (row / rows) * window.innerHeight + getRandomOffset();
-            sparkles.style.left = `${posX}px`;
-            sparkles.style.top = `${posY}px`;
-    
-            sparkesBackground.appendChild(sparkles);
+    const sparklesBackgrounds = document.getElementsByClassName("sparklesBackground")
+    for(var i = 0; i < sparklesBackgrounds.length; i++) {
+        const sparklesBackground = sparklesBackgrounds[i]
+        for (let row = 0; row < rows; row++) {
+            for (let col = 0; col < cols; col++) {
+                const sparkles = document.createElement('img');
+                sparkles.style.position = "absolute";
+                sparkles.src = "res/background_sparkles.png"
+                sparkles.classList.add('sparkles');
+        
+                // Define sparkles size
+                const size = 1;
+                sparkles.style.width = `${size}em`;
+                sparkles.style.height = `auto`;
+        
+                // Position the sparkles with random offset
+                const posX = (col / cols) * window.innerWidth + getRandomOffset();
+                const posY = (row / rows) * window.innerHeight + getRandomOffset();
+                sparkles.style.left = `${posX}px`;
+                sparkles.style.top = `${posY}px`;
+        
+                sparklesBackground.appendChild(sparkles);
+            }
         }
     }
 }
