3171f589aa21a8de31eb6f5b6e00ca0c2ad977c2
Maxime Maraval <46402524+MaximeJineMaraval@users.noreply.github.com>
2024-11-05 23:43:20 +0100

    feat: re-implem maps item dynamic visibility and click on map

--- main.js
+++ main.js
@@ -126,18 +126,31 @@ function fillPlanning(user) {
 }
 
 function fillMaps(user) {
-    /*const mapCityHallBlock = document.getElementById("mapCityHallBlock")
-    mapCityHallBlock.hidden = !user.is_invited_friday
+    document.getElementById("mapCityHallItem").addEventListener("click", function() {
+        window.open('https://maps.app.goo.gl/ifsNR4uJveJ7a8Vm8', '_blank')
+    })
+    document.getElementById("mapCastleItem").addEventListener("click", function() {
+        window.open('https://maps.app.goo.gl/dKrXLt6pFgMSy6Mr6', '_blank')
+    })
+
+    const mapCityHallBlock = document.getElementById("mapCityHallBlock")
+    if(user.is_invited_friday) {
+        mapCityHallBlock.style.display = "flex"
+    } else {
+        mapCityHallBlock.style.display = "none"
+    }
 
     const mapCastleTitle = document.getElementById("mapCastleTitle")
     if(user.is_invited_sunday) {
-        mapCastleTitle.textContent = "Samedi & Dimanche" 
+        mapCastleTitle.textContent = "Samedi et Dimanche" 
     } else {
         mapCastleTitle.textContent = "Samedi"
     }
 
-    const mapHostelBlock = document.getElementById("mapHostelBlock")
-    mapHostelBlock.hidden = !user.is_invited_sunday && !user.is_invited_full_saturday*/
+    const mapSleepTitle = document.getElementById("mapSleepTitle")
+    const mapSleepContent = document.getElementById("mapSleepContent")
+    mapSleepTitle.hidden = !user.is_invited_sunday && !user.is_invited_full_saturday
+    mapSleepContent.hidden = !user.is_invited_sunday && !user.is_invited_full_saturday
 }
 
 function fillForm(user1, user2, user3) {
