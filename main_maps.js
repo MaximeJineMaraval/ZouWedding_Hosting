61d0ef2fd2c355c4733e23282a02822c0f3c9f3c
Maxime Maraval <46402524+MaximeJineMaraval@users.noreply.github.com>
2024-12-03 20:09:50 +0100

    feat: update sleep section with links

--- main_maps.js
+++ main_maps.js
@@ -26,8 +26,6 @@ function fillMaps(user, isMultipleGuest) {
     // Show sleep section if needed
     const mapSleepTitle = document.getElementById("mapSleepTitle")
     const mapSleepContent = document.getElementById("mapSleepContent")
-    mapSleepTitle.hidden = !user.is_invited_sunday
-    mapSleepContent.hidden = !user.is_invited_sunday
     // Set sleep labels
     if(user.is_invited_to_sleep) {
         if(isMultipleGuest) {
@@ -37,7 +35,7 @@ function fillMaps(user, isMultipleGuest) {
             mapSleepTitle.textContent = "Et si tu dormais sur place avec nous ?"
             mapSleepContent.textContent = "Tu as été identifié dans la team des fêtards ! Pour te permettre de danser toute la nuit avec nous (et ce dès vendredi soir !), nous t'avons réservé un lit au château près de nous.\n\rUne partie des nuitées est prise en charge de notre côté, il restera 30€ par personne et par nuit à ta charge. Pense à nous indiquer dans le formulaire si tu dors sur place ou non."
         }
-    } else {
+    } else if(user.is_invited_sunday)  {
         // TODO Rajouter une liste de lien (faire un essai)
         mapSleepTitle.textContent = "Besoin de dormir dans les environs ?"
         if(isMultipleGuest) {
@@ -45,7 +43,22 @@ function fillMaps(user, isMultipleGuest) {
         } else {
             mapSleepContent.textContent = "Voici une liste de quelques gîtes et hôtels à proximité. N'hésite pas à regarder également sur Douai et Arras, les 2 grandes villes des environs."
         }
+        mapSleepContent.appendChild(getLinkFor("La Ferme de l'Abbaye", "https://www.ferme-abbaye.com/"))
+        mapSleepContent.appendChild(getLinkFor("La ferme de l'Ostrevent", "https://www.la-ferme-de-lostrevent.com/"))
+        mapSleepContent.appendChild(getLinkFor("Les chambres du moulin à Arleux", "https://www.google.com/search?q=Les+chambres+du+moulin+%C3%A0+Arleux&rlz=1C5CHFA_enFR1070FR1070&oq=Les+chambres+du+moulin+%C3%A0+Arleux&gs_lcrp=EgZjaHJvbWUyBggAEEUYOTIICAEQABgWGB4yCggCEAAYgAQYogQyCggDEAAYgAQYogQyCggEEAAYgAQYogQyCggFEAAYgAQYogQyBggGEEUYQNIBBzIyMWowajeoAgiwAgE&sourceid=chrome&ie=UTF-8"))
+        mapSleepContent.appendChild(getLinkFor("Hôtel l'Aquarium à Fresnes-lès-Montauban", "https://www.aquarium62.fr/"))
+    } else {
+        mapSleepTitle.hidden = true
+        mapSleepContent.hidden = true
     }
+}
+
+function getLinkFor(label, url) {
+    let link = document.createElement("a")
+    link.href = url
+    link.textContent = `\n\r${label}`
+    link.target = "_blank"
+    return link
 }
 
 window.fillMaps = fillMaps
